63b25cb9bec7284179ace149b8aea6866f77e558
V-Karch <luna.karch05@gmail.com>
2025-07-14 08:05:23 -0400

    cogs/roles.py: added sample command for listing a users roles (nonfunctional)

--- cogs/roles.py
+++ cogs/roles.py
@@ -10,12 +10,21 @@ class Roles(commands.Cog):
     roles_group = app_commands.Group(name="role", description="role commands")
 
     @roles_group.command(name="give", description="give a role to a user")
+    @app_commands.describe(
+        user="A server member to give a role to",
+        role="A server role to apply to a server member",
+    )
     async def give_role(
         self, interaction: discord.Interaction, user: discord.Member, role: discord.Role
     ) -> None:
         await interaction.response.defer()
         await user.add_roles(role)
         await interaction.followup.send(f'Given role "{role}" to "{user}"')
+
+    @roles_group.command(name="list", description="list all roles a user has")
+    @app_commands.describe(user="A server member whose roles you want to see")
+    async def list_roles(self, interaction: discord.Interaction, user: discord.Member):
+        await interaction.response.send_message(user.mention)
 
 
 async def setup(bot: commands.Bot):
